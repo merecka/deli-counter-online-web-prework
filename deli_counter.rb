629f4613e4dfdb6db6755dc140c70c2ecf18b22a
Alex Merecka <merecka@gmail.com>
2019-02-05 00:05:49 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(katz_deli)
   current_line = []
   if katz_deli.length > 0
    katz_deli.each_with_index do |person, index|
-    current_line.push("The line is currently: #{index+1}. #{person}")
-    puts current_line
+    current_line.push("#{index+1}. #{person}")
     end
+    puts "The line is currently: #{current_line}"
   else
     puts "The line is currently empty."
   end

ad40cb403f152d789abfa8fcaac9898939d5bacb
Alex Merecka <merecka@gmail.com>
2019-03-19 18:35:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -30,5 +30,14 @@ end
 #     puts "There is nobody waiting to be served!"
 #   end
 # end
+
+def now_serving(katz_deli)
+  if katz_deli.length > 0
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
     
     
